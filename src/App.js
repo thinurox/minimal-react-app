46f41c57e61bce980c3178fa687d04c39b49e761
thinuwan <thinuwan.wickramaarachci@pearson.com>
2018-02-01 10:24:49 +0530

    added dimple fetch API request

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import React from 'react'
 class App extends React.Component {
     constructor (props) {
         super(props)
+
+        fetch('http://localhost:8082/api')
+            .then(response => {
+                if (response.status != 200) {
+                    throw new Error('Bad response from server');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            });
+
     }
 
     render () {
